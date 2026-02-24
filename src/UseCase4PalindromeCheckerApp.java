9a3b0b8cbd5d33d2d47b9a05b22b86333bd190ae
harshalchaprot1 <harshalchaprot1@gmail.com>
2026-02-24 15:58:34 +0530

    Added UC4: Palindrome check using reverse string

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "level";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        boolean isPalindrome = input.equals(reversed);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Reversed text: " + reversed);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
